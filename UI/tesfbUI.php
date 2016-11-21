4539c84eeffa8aabb74f2700ef7db7b26f9cd82f
Ega Prianto <egaprianto85@gmail.com>
2016-11-21 16:48:28 +0700

    update UI beres tinggal masukkin twitter

--- UI/tesfbUI.php
+++ UI/tesfbUI.php
@@ -8,8 +8,10 @@ $MCF = new MyCurlFacebook();
 <head>
   <meta charset="utf-8">
   <title>LBW-dapet-A</title>
-  <meta name="generator" content="Google Web Designer 1.6.4.1010">
-  <style type="text/css" id="gwd-text-style">
+  <!--meta name="generator" content="Google Web Designer 1.6.4.1010"-->
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <!--style type="text/css" id="gwd-text-style">
     p {
       margin: 0px;
     }
@@ -22,8 +24,30 @@ $MCF = new MyCurlFacebook();
     h3 {
       margin: 0px;
     }
-  </style>
+  </style-->
   <style type="text/css">
+    a {
+      color: inherit;
+    }
+
+    a:link {
+        text-decoration: none;
+    }
+
+    a:visited {
+        text-decoration: none;
+    }
+
+    a:hover {
+      color: default;
+
+        text-decoration: none;
+    }
+
+    a:active {
+        text-decoration: none;
+    }
+
     html, body {
       width: 100%;
       height: 100%;
@@ -37,17 +61,15 @@ $MCF = new MyCurlFacebook();
     .pp-div {
       width: 100px;
       height: 100px;
+      border-radius: 50px;
       display: inline-block;
       vertical-align: middle;
       margin-right: 20%;
     }
     .header-div {
-      height: 170px;
-      width: 85%;
-      left: 798px;
-      top: 171px;
       padding-top: 2%;
       padding-left: 15%;
+      padding-bottom: 15px;
       background-color: white;
     }
     .profilename-div {
@@ -56,10 +78,8 @@ $MCF = new MyCurlFacebook();
       height: 37px;
       left: 123px;
       display: inline-block;
+      margin-bottom: 0px;
       top: 55px;
-    }
-    .ppcover-div {
-      position: fixed;
     }
     .profileandlikes-div {
       display: inline-block;
@@ -68,12 +88,12 @@ $MCF = new MyCurlFacebook();
       top: 3296px;
     }
     .twitter-follow {
-      font-family: 'Arial Black';
+  font: bold 14px/18px Helvetica, Arial, sans-serif;
       color: white;
       padding: 30px 50px;
       margin: 2px;
       border-radius: 5px;
-      background-color: rgb(64, 153, 255);
+      background-color: #55ACEE;
     }
     .facebook-like {
       font-family: 'Arial Black';
@@ -86,13 +106,69 @@ $MCF = new MyCurlFacebook();
     .tagppl-div {
       color: rgb(128, 128, 128);
     }
+    .twitter-feed{    
+      padding-left: 5%;  
+      padding-right: 0px;   
+      padding-top: 0px;            
+      border-right: 2px solid;
+      border-color: #ffffff;
+      background-color: rgb(239, 239, 239);
+    }
+    .facebook-feed{
+      padding-right: 5%; 
+      padding-left:0px;   
+      padding-top: 0px;      
+      border-left: 2px solid;
+      border-color: #ffffff;
+      background-color: rgb(239, 239, 239);
+    }
+
+    .facebook-post{
+      margin-left: 2.5%;
+      margin: 2%;
+      padding:2%;
+      background-color: white;
+    }
+    .twitter-post{
+      margin-right: 2.5%;
+      background-color: white;
+    }
+    #twitter-banner {
+      float: right;
+      border-radius: 0% 0% 0% 100%;
+      margin: 0px;
+      padding:15px;
+      padding-left: 35px;
+      padding-bottom: 30px;
+      font: bold 18px Helvetica, Arial, sans-serif;
+      background-color:  #55ACEE; 
+      color:#f5f8fa;
+    }
+    #facebook-banner {
+      float: left;
+      border-radius: 0% 0% 100% 0%;
+      margin: 0px;
+      padding:15px;
+      padding-right: 35px;
+      padding-bottom: 30px;
+      font: bold 18px Helvetica, Arial, sans-serif;
+      background-color:  rgb(59, 89, 152); 
+      color:#f5f8fa;
+    }
+    .facebook-post-message{
+      padding-top: 2%;
+      padding-bottom: 3%;
+    }
+    .facebook-post-time{
+      text-align: right;
+      margin-top: 3%;
+    }
   </style>
 </head>
 
 <body>
-  <div class="header-div">
+  <div class="header-div col-md-12">
     <div class="profileandlikes-div">
-      <img src="assets/layer.png" class="ppcover-div pp-div">
       <img src="
 <?php 
         echo $MCF->generateURL("1597591840496775/picture","width=100");
@@ -103,7 +179,17 @@ $MCF = new MyCurlFacebook();
       </a>
       <a class="facebook-like">
       <?php 
-        echo $MCF->get("1597591840496775","fields=fan_count")->fan_count;
+        $countLikes = $MCF->get("1597591840496775","fields=fan_count")->fan_count;
+        if($countLikes < 1000){
+            $countLikes;
+        }
+        else if($countLikes >= 1000 && $countLikes < 1000000){
+            echo sprintf("%d K", $countLikes/1000);
+        }
+        else{
+            echo sprintf("%d M", $countLikes/1000000);
+
+        }
       ?>
 
         Likes 
@@ -120,6 +206,52 @@ $MCF = new MyCurlFacebook();
      @Maghfirare     
 
     </a>
+  </div>
+  <div class = "twitter-feed col-md-6 col-sm-6">
+    <h2 id="twitter-banner">twitter</h2>
+    <div class = "twitter-post col-md-11 col-sm-11">
+
+    </div>
+  </div>
+  <div class = "facebook-feed col-md-6 col-sm-6">
+    <h2 id="facebook-banner">facebook</h2>
+    <div class = 'facebook-post col-md-11 col-sm-11 pull-right'>
+      <div class = 'facebook-post-message col-md-12 col-sm-12'>
+
+      </div>
+      <img src = 'assets/picture/tes.jpg' class = 'col-md-12 col-sm-12'>
+      <div class = 'facebook-post-time pull-right col-md-6 col-sm-6'>
+
+      </div>
+    </div>
+    <?php
+       $post = $MCF->get("1597591840496775/feed","")->data;
+       for($i = 0 ; $i<sizeof($post);$i++){
+          $curPost =  $post[$i];
+          echo "<div class = 'facebook-post col-md-11 col-sm-11 pull-right'>";
+          echo "<div class = 'facebook-post-message col-md-12 col-sm-12'>";
+          if(property_exists($curPost, 'message')){
+            echo $curPost->message.'<br>';
+          }else if(property_exists($curPost, 'story')){
+            echo $curPost->story.'<br>';
+          }
+          echo "</div>";
+          echo "<img src = '";
+          if (property_exists($curPost, 'id')) {
+             $curPicture = $MCF->get($curPost->id,"fields=full_picture")->full_picture;
+             echo $curPicture;
+          }
+          echo "' class = 'col-md-12 col-sm-12'>";
+
+          echo "<div class = 'facebook-post-time pull-right col-md-6 col-sm-6'>";
+          if(property_exists($curPost, 'created_time')){
+            echo substr($curPost->created_time, 0,10).' pukul '.substr($curPost->created_time, 11,5).'<br>';
+          }
+          echo"</div>";
+          echo"</div>";
+       }
+    ?>
+
   </div>
 </body>
 
