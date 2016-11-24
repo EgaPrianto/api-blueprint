9408d8c1712a9da66ab8a1e7d81f2461e5b13367
Ega Prianto <egaprianto85@gmail.com>
2016-11-24 10:52:30 +0700

    masukkin class final

---
+++ Facebook/MyCurlFacebook.php
@@ -0,0 +1,66 @@
+<?php 
+
+
+class MyCurlFacebook {
+
+
+    const GRAPH_VERSION = 'v2.8';
+
+    private $hostname = 'https://graph.facebook.com/';
+    private $access_token = 'access_token=';
+    private $curl;
+
+    public function __construct(){
+        $this->curl = curl_init();
+        $execQuery = 'https://graph.facebook.com/v2.8/oauth/access_token?client_id={client_id}&client_secret={client_secret}&grant_type=client_credentials';
+        curl_setopt($this->curl, CURLOPT_URL, $execQuery);
+        curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($this->curl);
+        curl_close($this->curl);
+        $obj = json_decode($response);
+        $this->access_token = $this->access_token.$obj->{'access_token'};
+    }
+
+    public function generateURL($path, $query){
+        $execQuery = $this->hostname.static::GRAPH_VERSION."/".$path."?".$this->access_token;
+        $this->curl = curl_init();
+        if ($query !== null && $query !== "" ) {
+            $execQuery .= "&".$query;
+        }
+        return $execQuery;
+    }
+
+
+    public function get($path, $query){
+        $execQuery = $this->generateURL($path, $query);
+        curl_setopt($this->curl, CURLOPT_URL, $execQuery);
+        curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($this->curl);
+        $result =  json_decode($response);
+        curl_close($this->curl);
+        return $result;
+    }
+}
+/*
+require_once __DIR__ . '/vendor/autoload.php';  
+if(!session_id()) {
+    session_start();
+}
+$fb = new Facebook\Facebook([
+  'app_id' => '189062984870016', // Replace {app-id} with your app id
+  'app_secret' => '46aa26be20511e7e76179da35790b2fe',
+  'default_graph_version' => 'v2.8',
+  ]);
+// Since all the requests will be sent on behalf of the same user,
+// we'll set the default fallback access token here.
+$fb->setDefaultAccessToken('189062984870016|7MBHU8Zl7IIEaahMbN-VZZxNrpM');
+
+  $response = $fb->get('/1597591840496775');
+
+
+echo "<p> HTTP status code: " . $response->getHttpStatusCode() . "<br />\n";
+echo "Response: " . $response->getBody() . "</p>\n\n";
+echo "<hr />\n\n";
+*/
